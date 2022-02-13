c00c09fc4370f2d7955c2c57e336181b2c23b1c3
Bruno Queiroz <bruno.queiroz@liferay.com>
2022-02-13 17:21:47 -0300

    Project created

---
+++ src/main/java/com/project/weblux/WebluxApplication.java
@@ -0,0 +1,16 @@
+package com.project.weblux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebluxApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(WebluxApplication.class, args);
+
+        System.out.println("Hello From World!!!");
+    }
+
+}
---
+++ src/test/java/com/project/weblux/WebluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.weblux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
